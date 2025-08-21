0323ce64955ba8ee19ed2a3c27ec82529d65ebff
Naman Tiwari <namantiwari142@gmail.com>
2025-08-21 19:29:37 +0530

    Taking input file updated

--- Templates/takingInput.java
+++ Templates/takingInput.java
@@ -1,1 +1,34 @@
+package myutils;
 
+import java.io.*;
+import java.util.*;
+
+public class FastInput {
+    private BufferedReader br;
+    private StringTokenizer st;
+
+    public FastInput() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public String next() throws IOException {
+        while (st == null || !st.hasMoreElements()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    public int nextInt() throws IOException { 
+        return Integer.parseInt(next()); 
+    }
+
+    public long nextLong() throws IOException { 
+        return Long.parseLong(next()); 
+    }
+
+    public int[] readArray(int n) throws IOException {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = nextInt();
+        return arr;
+    }
+}
